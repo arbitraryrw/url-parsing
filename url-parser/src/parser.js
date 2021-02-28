fde24d0537764e7723739304fe0b7b5d248b2890
arbitraryrw <nickcucakovic@gmail.com>
2021-02-28 15:24:45 +0000

    Creating basic boiler plate helper function for demoing URL parsing

--- url-parser/src/parser.js
+++ url-parser/src/parser.js
@@ -1,3 +1,27 @@
-exports.handleUrl = function (url) {
-    return url;
+const { URL } = require('url');
+
+
+
+exports.handleAbsoluteUrl = function () {
+    var url = 'https://user:pass@sub.host.com:8080\\\\p/a/t/h?query=string#has'
+    var newUrl = new URL(url);
+    console.log(newUrl);
+    return newUrl;
+  };
+
+
+exports.handleRelativeUrl = function () {
+    var url = 'https://user:pass@sub.host.com:8080\\\\p/a/t/h?query=string#has'
+    var newUrl = new URL(url);
+    console.log(newUrl);
+    return newUrl;
+  };
+
+
+exports.constructUrl = function () {
+    var base = 'http://127.0.0.1'
+    var relative = '/p/a/t/h?query=string#has'
+    var newUrl = new URL(relative, base);
+    console.log(newUrl);
+    return newUrl;
   };
