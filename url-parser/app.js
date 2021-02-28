844c58f9729430feca98e2311edc8c6b65a8ffe3
arbitraryrw <nickcucakovic@gmail.com>
2021-02-28 15:24:06 +0000

    Basic project redirect flow added to main app.js file

--- url-parser/app.js
+++ url-parser/app.js
@@ -1,14 +1,36 @@
 const http = require('http');
+const { URL } = require('url');
+
 const parser = require('./src/parser')
+const util = require('./src/util')
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const baseUrl = 'http://' + hostname + ':' + port
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  res.write("Demoing URL: " + parser.handleUrl("blah"));
+  var url = baseUrl + req.url;
+  var parsedUrl = new URL(url);
+
+  var nextUrl = parsedUrl.searchParams.get('nextUrl');
+
+  //1. normalize
+  //2. sanitize
+  //3. validate
+
+  // res.write("Demoing URL: " + parser.handleRelativeUrl());
+  // res.write("Demoing URL: " + parser.constructUrl());
+
+
+  if(nextUrl != null){
+    redirectLocation = baseUrl + "/" + nextUrl;
+    res = util.setLocationHeader(res, redirectLocation);
+  }
+
+  res.write("Done!");
   res.end();
 });
 
