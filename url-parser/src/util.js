c75e5af58cfe7df276cb7036aaff5db93129f08c
arbitraryrw <nickcucakovic@gmail.com>
2021-02-28 15:25:31 +0000

    Creating Util.js file to store common helper functions and adding redirect function

---
+++ url-parser/src/util.js
@@ -0,0 +1,8 @@
+exports.setLocationHeader = function (res, url) {
+    console.log("Redirecting to " + redirectLocation);
+    res.writeHead(302 , {
+        'Location' : url
+    });
+
+    return res
+  };
