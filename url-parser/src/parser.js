934beb7bd93bc0af8d7daf23bc4da6aec608a99f
arbitraryrw <nickcucakovic@gmail.com>
2021-03-07 11:42:15 +0000

    Utility validation function for URLs added to parser

--- url-parser/src/parser.js
+++ url-parser/src/parser.js
@@ -1,27 +1,29 @@
 const { URL } = require('url');
 
+exports.isUrlValid = function (relUrl) {
 
+    console.log("Handling: " + relUrl)
 
-exports.handleAbsoluteUrl = function () {
-    var url = 'https://user:pass@sub.host.com:8080\\\\p/a/t/h?query=string#has'
-    var newUrl = new URL(url);
-    console.log(newUrl);
-    return newUrl;
+    var re = new RegExp("^(\/[a-zA-Z0-9]+){0,}$");
+
+    if (re.test(relUrl)) {
+      return true
+    } else {
+      return false
+    }
   };
 
-
+exports.handleAbsoluteUrl = function () {
+      var url = 'https://user:pass@sub.host.com:8080\\\\p/a/t/h?query=string#has'
+      var newUrl = new URL(url);
+      console.log(newUrl);
+      return newUrl;
+    };
+  
+  
 exports.handleRelativeUrl = function () {
     var url = 'https://user:pass@sub.host.com:8080\\\\p/a/t/h?query=string#has'
     var newUrl = new URL(url);
-    console.log(newUrl);
-    return newUrl;
-  };
-
-
-exports.constructUrl = function () {
-    var base = 'http://127.0.0.1'
-    var relative = '/p/a/t/h?query=string#has'
-    var newUrl = new URL(relative, base);
     console.log(newUrl);
     return newUrl;
   };
